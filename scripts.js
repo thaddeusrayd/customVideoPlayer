fdb2c7928ec74b357b064d38b98d3bec30511ef6
thaddeusrayd <thaddeusrayd@gmail.com>
2020-04-30 14:57:24 -0500

    add skip and handleRangeUpdate functions with event listeners

--- scripts.js
+++ scripts.js
@@ -26,9 +26,19 @@ function updateButton() {
     console.log('Update the button');
 }
 
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    console.log(this.value);
+}
 /* hook up event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
